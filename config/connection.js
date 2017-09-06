84530de6af70f5813e42a7619fa15c92d2571c2d
rlawton512 <rocsi54321@gmail.com>
2017-09-05 21:17:48 -0400

    submissionv.2

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,17 @@
 //set up code to connect Node to MySQL 
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "test1",
-  database: "burgers_db"
-});
-
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+  } else {
+    connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "test1",
+    database: "burgers_db"
+  });
+};
 //make connection
 connection.connect(function(err) {
   if (err) {
